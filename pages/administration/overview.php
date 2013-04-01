b39b2df5b69be68e5bbf6174b918e6cce69b78dd
Georg Limbach (Warnow) <georf@georf.de>
2013-04-01 17:05:04 +0200

    find bad persons

--- pages/administration/overview.php
+++ pages/administration/overview.php
@@ -8,7 +8,7 @@
     <li><a href="?page=administration&amp;admin=users">Users</a></li>
     <li><a href="?page=administration&amp;admin=logs">Logs</a></li>
     <li><a href="?page=administration&amp;admin=errors">Errors</a></li>
-    <li><a href="?page=administration&amp;admin=find_bad_persons">Find bad persons</a></li>
+    <li><a href="?page=administration&amp;admin=find_bad_persons">Find bad persons</a> - <a href="?page=administration&amp;admin=find_bad_persons2">Find same persons</a></li>
     <li><a href="?page=administration&amp;admin=team_logo">Team Logo</a> <a href="?page=administration&amp;admin=check_team_logo">Check</a></li>
     <li><a href="?page=administration&amp;admin=news">News</a></li>
     <li><a href="?page=administration&amp;admin=clean">Clean cache</a></li>
