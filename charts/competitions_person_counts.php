3aa4b62386ea79f53cc3060e03290450273df1a5
Georg Limbach (Warnow) <georf@georf.de>
2013-06-14 14:31:59 +0200

    add new charts

---
+++ charts/competitions_person_counts.php
@@ -0,0 +1,131 @@
+<?php
+
+TempDB::generate('x_scores_hbf');
+TempDB::generate('x_scores_hbm');
+TempDB::generate('x_scores_hl');
+
+$fullData = Cache::get();
+
+if (!$fullData) {
+
+    if (Check::get('event') && Check::isIn($_GET['event'], 'events')) {
+        $competitions = $db->getRows("
+            SELECT `id`
+            FROM `competitions`
+            WHERE `event_id` = '".$db->escape($_GET['event'])."'
+        ");
+    } elseif (Check::get('place') && Check::isIn($_GET['place'], 'places')) {
+        $competitions = $db->getRows("
+            SELECT `id`
+            FROM `competitions`
+            WHERE `place_id` = '".$db->escape($_GET['place'])."'
+        ");
+    } else {
+        $competitions = $db->getRows("
+            SELECT `id`
+            FROM `competitions`
+        ");
+    }
+    
+    $counts = array( 0, 0, 0, 0, 0, 0);
+    $labels = array(
+        'Keine',
+        '1 bis 25',
+        '25 bis 50',
+        '50 bis 75',
+        '75 bis 100',
+        '> 100'
+    );
+    
+    foreach ($competitions as $competition) {
+    
+        $count = $db->getFirstRow("
+            SELECT COUNT(*) AS `count`
+            FROM (
+                SELECT `person_id`
+                FROM `scores`
+                WHERE `competition_id` = '".$competition['id']."'
+                GROUP BY `person_id`
+            ) `i`
+        ", 'count');
+        
+        if ($count <= 0) $counts[0]++;
+        elseif ($count < 26) $counts[1]++;
+        elseif ($count < 51) $counts[2]++;
+        elseif ($count < 76) $counts[3]++;
+        elseif ($count < 101) $counts[4]++;
+        else $counts[5]++;
+    }
+    
+    $MyData = new pData();
+    $MyData->addPoints($counts, "time");
+    $MyData->addPoints($labels, "Platzierung");
+    $MyData->setAbscissa("Platzierung");
+
+    $fullData = array(
+        'myData' => $MyData
+    );
+    Cache::put($fullData);
+}
+
+$MyData = $fullData['myData'];
+
+
+/* Create the cache object */
+$MyCache = new pCache();
+
+/* Compute the hash linked to the chart data */
+$ChartHash = $MyCache->getHash($MyData, Cache::getId());
+
+
+/* Test if we got this hash in our cache already */
+if ($MyCache->isInCache($ChartHash)) {
+
+    /* If we have it, get the picture from the cache! */
+    $MyCache->strokeFromCache($ChartHash);
+} else {
+
+
+
+    $w = 176;
+    $h = 110;
+    $myPicture = Chart::create($w, $h, $MyData);
+
+    /* Turn of Antialiasing */
+    $myPicture->Antialias = TRUE;
+
+    /* Set the default font */
+    $myPicture->setFontProperties(array(
+        "FontName"=>PCHARTDIR."fonts/UbuntuMono-R.ttf",
+        "FontSize"=>Chart::size(9),
+        "R"=>0,
+        "G"=>0,
+        "B"=>0
+    ));
+
+    /* Create the pPie object */
+    $PieChart = new pPie($myPicture, $MyData);
+
+    /* Draw a simple pie chart */
+    $PieChart->draw2DPie(Chart::size(50),Chart::size(50), array(
+        "WriteValues"=>PIE_VALUE_PERCENTAGE,
+        "ValueR"=>50,
+        "ValueG"=>50,
+        "ValueB"=>50,
+        "ValueAlpha"=>100,
+        "Border"=>TRUE,
+        "ValuePosition"=>PIE_VALUE_INSIDE,
+        "SkewFactor"=>0.5,
+        "Radius"=>Chart::size(49),
+        "ValuePadding"=>Chart::size(18),
+        "LabelStacked"=>true
+    ));
+
+    $PieChart->drawPieLegend(Chart::size(98),Chart::size(17));
+
+    /* Push the rendered picture to the cache */
+    $MyCache->writeToCache($ChartHash, $myPicture);
+
+    /* Render the picture */
+    $myPicture->stroke();
+}
---
+++ charts/competitions_team_counts.php
@@ -0,0 +1,144 @@
+<?php
+
+TempDB::generate('x_scores_hbf');
+TempDB::generate('x_scores_hbm');
+TempDB::generate('x_scores_hl');
+
+$fullData = Cache::get();
+
+if (!$fullData) {
+
+    if (Check::get('event') && Check::isIn($_GET['event'], 'events')) {
+        $competitions = $db->getRows("
+            SELECT `id`
+            FROM `competitions`
+            WHERE `event_id` = '".$db->escape($_GET['event'])."'
+        ");
+    } elseif (Check::get('place') && Check::isIn($_GET['place'], 'places')) {
+        $competitions = $db->getRows("
+            SELECT `id`
+            FROM `competitions`
+            WHERE `place_id` = '".$db->escape($_GET['place'])."'
+        ");
+    } else {
+        $competitions = $db->getRows("
+            SELECT `id`
+            FROM `competitions`
+        ");
+    }
+    
+    $counts = array( 0, 0, 0, 0, 0);
+    $labels = array(
+        'Keine',
+        '1 bis 10',
+        '11 bis 20',
+        '21 bis 30',
+        '> 30',
+    );
+    
+    foreach ($competitions as $competition) {
+    
+        $count = $db->getFirstRow("
+            SELECT COUNT(*) AS `count`
+            FROM (
+                SELECT `team`
+                FROM (
+                    SELECT CONCAT(CAST(`team_id` AS CHAR),`sex`,CAST(`team_number` AS CHAR)) AS `team`
+                    FROM `scores_la`
+                    WHERE `competition_id` = '".$competition['id']."'
+                UNION
+                    SELECT CONCAT(CAST(`team_id` AS CHAR),`sex`,CAST(`team_number` AS CHAR)) AS `team`
+                    FROM `scores_fs`
+                    WHERE `competition_id` = '".$competition['id']."'
+                UNION
+                    SELECT CONCAT(CAST(`team_id` AS CHAR),'female',CAST(`team_number` AS CHAR)) AS `team`
+                    FROM `scores_gs`
+                    WHERE `competition_id` = '".$competition['id']."'
+                UNION
+                    SELECT CONCAT(CAST(`team_id` AS CHAR),`pi`.`sex`,CAST(`team_number` AS CHAR)) AS `team`
+                    FROM `scores` `si`
+                    INNER JOIN `persons` `pi` ON `si`.`person_id` = `pi`.`id`
+                    WHERE `si`.`competition_id` = '".$competition['id']."'
+                ) `i`
+                GROUP BY `i`.`team`
+            ) `i2`
+        ", 'count');
+        if ($count <= 0) $counts[0]++;
+        elseif ($count < 11) $counts[1]++;
+        elseif ($count < 21) $counts[2]++;
+        elseif ($count < 31) $counts[3]++;
+        else $counts[4]++;
+    }
+    
+    $MyData = new pData();
+    $MyData->addPoints($counts, "time");
+    $MyData->addPoints($labels, "Platzierung");
+    $MyData->setAbscissa("Platzierung");
+
+    $fullData = array(
+        'myData' => $MyData
+    );
+    Cache::put($fullData);
+}
+
+$MyData = $fullData['myData'];
+
+
+/* Create the cache object */
+$MyCache = new pCache();
+
+/* Compute the hash linked to the chart data */
+$ChartHash = $MyCache->getHash($MyData, Cache::getId());
+
+
+/* Test if we got this hash in our cache already */
+if ($MyCache->isInCache($ChartHash)) {
+
+    /* If we have it, get the picture from the cache! */
+    $MyCache->strokeFromCache($ChartHash);
+} else {
+
+
+
+    $w = 170;
+    $h = 110;
+    $myPicture = Chart::create($w, $h, $MyData);
+
+    /* Turn of Antialiasing */
+    $myPicture->Antialias = TRUE;
+
+    /* Set the default font */
+    $myPicture->setFontProperties(array(
+        "FontName"=>PCHARTDIR."fonts/UbuntuMono-R.ttf",
+        "FontSize"=>Chart::size(9),
+        "R"=>0,
+        "G"=>0,
+        "B"=>0
+    ));
+
+    /* Create the pPie object */
+    $PieChart = new pPie($myPicture, $MyData);
+
+    /* Draw a simple pie chart */
+    $PieChart->draw2DPie(Chart::size(50),Chart::size(50), array(
+        "WriteValues"=>PIE_VALUE_PERCENTAGE,
+        "ValueR"=>50,
+        "ValueG"=>50,
+        "ValueB"=>50,
+        "ValueAlpha"=>100,
+        "Border"=>TRUE,
+        "ValuePosition"=>PIE_VALUE_INSIDE,
+        "SkewFactor"=>0.5,
+        "Radius"=>Chart::size(49),
+        "ValuePadding"=>Chart::size(18),
+        "LabelStacked"=>true
+    ));
+
+    $PieChart->drawPieLegend(Chart::size(98),Chart::size(17));
+
+    /* Push the rendered picture to the cache */
+    $MyCache->writeToCache($ChartHash, $myPicture);
+
+    /* Render the picture */
+    $myPicture->stroke();
+}
