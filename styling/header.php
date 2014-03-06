30e575f3ee62a06d3cebcb9effafdaddcf7af3f4
Georg Limbach (Warnow) <georf@georf.de>
2014-03-06 22:01:22 +0100

    Add link to logs

--- styling/header.php
+++ styling/header.php
@@ -14,44 +14,39 @@
 </head>
 <body>
 <div id="wrap">
-  <div class="navbar navbar-default navbar-static-top" role="navigation">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-      </div>
-      <div class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-          <li><a href="/page/home.html">Startseite</a></li>
-          <li><a href="/page/dates.html">Termine</a></li>
-          <li><a href="/page/dcup.html">D-Cup</a></li>
-          <li><a href="/page/news.html">Neuigkeiten</a></li>
-          <li><a href="/page/competitions.html">Wettkämpfe</a></li>
-          <li><a href="/page/teams.html">Mannschaften</a></li>
-          <li><a href="/page/persons.html">Personen</a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Weitere Statistiken <b class="caret"></b></a>
-            <ul class="dropdown-menu">
-              <li><a href="/page/places.html">Orte</a></li>
-              <li><a href="/page/events.html">Typen</a></li>
-              <li><a href="/page/years.html">Jahre</a></li>
-              <li class="divider"></li>
-              <li class="dropdown-header">Interessantes</li>
-              <li><a href="/page/year-2013.html">Statistiken 2013</a></li>
-              <li><a href="#">Letzte Änderungen</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
-
-    <!-- Begin page content -->
-    <div class="container">
-   
-
-    
+<div class="navbar navbar-default navbar-static-top" role="navigation">
+<div class="container">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+<span class="sr-only">Toggle navigation</span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+</div>
+<div class="collapse navbar-collapse">
+<ul class="nav navbar-nav">
+<li><a href="/page/home.html">Startseite</a></li>
+<li><a href="/page/dates.html">Termine</a></li>
+<li><a href="/page/dcup.html">D-Cup</a></li>
+<li><a href="/page/news.html">Neuigkeiten</a></li>
+<li><a href="/page/competitions.html">Wettkämpfe</a></li>
+<li><a href="/page/teams.html">Mannschaften</a></li>
+<li><a href="/page/persons.html">Personen</a></li>
+<li class="dropdown">
+<a href="#" class="dropdown-toggle" data-toggle="dropdown">Weitere Statistiken <b class="caret"></b></a>
+<ul class="dropdown-menu">
+<li><a href="/page/places.html">Orte</a></li>
+<li><a href="/page/events.html">Typen</a></li>
+<li><a href="/page/years.html">Jahre</a></li>
+<li class="divider"></li>
+<li class="dropdown-header">Interessantes</li>
+<li><a href="/page/year-2013.html">Statistiken 2013</a></li>
+<li><a href="/page/logs.html">Letzte Änderungen</a></li>
+</ul>
+</li>
+</ul>
+</div>
+</div>
+</div>
+<div class="container">
