aa1a609e482e859e1f698f4276fb5b3a29ecdd74
Georg Limbach (Warnow) <georf@georf.de>
2014-05-28 00:11:53 +0200

    Loggen von Wettkämpfern korrigieren
    closes #37

--- includes/api/set/score-wk.php
+++ includes/api/set/score-wk.php
@@ -21,23 +21,25 @@ switch ($discipline) {
 $score = Check2::except()->post('scoreId')->isIn("scores_".$table, 'row');
 
 $update = false;
+$updateSqlSelect = array();
+$updateSqlJoin = array();
 for ($i = 1; $i < $wks; $i++) {
   $participation = $db->getFirstRow("
     SELECT `id`,`person_id`
     FROM `person_participations_".$table."`
     WHERE `score_id` = ".$score['id']."
     AND `position` = ".$i."
-    LIMIT 1", 'id');
+    LIMIT 1");
   $personId = Check2::value()->post('person'.$i)->isIn('persons', true);
   if (!$personId && Check2::boolean()->post('person'.$i)->match('|^NULL$|') && $participation !== false) {
     $db->deleteRow("person_participations_".$table, $participation['id']);
     $update = true;
-  } elseif ($personId !== false && $participation !== false && $participation['person_id'] != $personId) {
+  } elseif ($personId !== null && $participation !== false && $participation['person_id'] != $personId) {
     $db->updateRow("person_participations_".$table, $participation['id'], array(
       'person_id' => $personId
     ));
     $update = true;
-  } elseif ($personId !== false && $participation === false) {
+  } elseif ($personId !== null && $participation === false) {
     $db->insertRow("person_participations_".$table, array(
       'person_id' => $personId,
       'score_id' => $score['id'],
@@ -45,10 +47,18 @@ for ($i = 1; $i < $wks; $i++) {
     ));
     $update = true;
   }
+  $updateSqlSelect[] = "`p".$i."`.`person_id` AS `person_".$i."`";
+  $updateSqlJoin[] = "LEFT JOIN `person_participations_".$table."` `p".$i."` ON `p".$i."`.`score_id` =  `s`.`id` AND `p".$i."`.`position` = ".$i." ";
 }
 
 if ($update) {
-  $score = FSS::tableRow("scores_".$table, $score['id']);
+  $score = $db->getFirstRow("
+    SELECT 
+    `s`.`id`,`team_id`,`team_number`,`competition_id`,`time`,
+    ".implode(", ", $updateSqlSelect)."
+    FROM `scores_gs` `s`
+    ".implode($updateSqlJoin)."
+    WHERE `s`.`id` = '".$score['id']."'");
   Log::insert('set-score-wk', array(
     'key' => $discipline,
     'score' => $score,
