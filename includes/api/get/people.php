67e9a1d45c209c8956b77bb78cb03f9202084c2a
Georg Limbach (Warnow) <georf@georf.de>
2015-07-23 13:28:20 +0200

    U20 in diesem Jahr mit ausgeben

--- includes/api/get/people.php
+++ includes/api/get/people.php
@@ -1,3 +1,10 @@
 <?php
-$output['people'] = $db->getRows("SELECT * FROM `persons`");
+$output['people'] = $db->getRows("SELECT *,
+    EXISTS (
+      SELECT 1 
+      FROM scores_dcup_single_u u 
+      INNER JOIN scores s ON s.id = u.score_id
+      INNER JOIN dcups d ON d.id = u.dcup_id AND d.year = YEAR(NOW())
+      WHERE s.person_id = persons.id 
+    ) AS youth FROM `persons`");
 $output['success'] = true;
--- includes/api/get/persons.php
+++ includes/api/get/persons.php
@@ -7,7 +7,14 @@ if ($sex) {
 }
 
 $output['persons'] = $db->getRows("
-  SELECT `name`,`firstname`,`id`,`sex`
+  SELECT `name`,`firstname`,`id`,`sex`,
+    EXISTS (
+      SELECT 1 
+      FROM scores_dcup_single_u u 
+      INNER JOIN scores s ON s.id = u.score_id
+      INNER JOIN dcups d ON d.id = u.dcup_id AND d.year = YEAR(NOW())
+      WHERE s.person_id = persons.id 
+    ) AS youth
   FROM `persons`
   ".$where."
   ORDER BY `name`, `firstname`
